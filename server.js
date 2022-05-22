6ef860a0a3c801978fe012e4474469b1e062cde8
Kym Reilly <kymreilly@hotmail.com>
2022-05-22 10:15:13 +0930

    Localhost PORT listen added

--- server.js
+++ server.js
@@ -16,3 +16,7 @@ app.use(express.static("public"));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
