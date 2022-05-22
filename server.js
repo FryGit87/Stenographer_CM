97f8b1372b086e31d0df017c26fea670175e3261
Kym Reilly <kymreilly@hotmail.com>
2022-05-23 07:34:53 +0930

    Code comments added and redundant code removed

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
+// Dependencies
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
-const {
-  readFromFile,
-  readAndAppend,
-  writeToFile,
-} = require("./helpers/fsUtils");
+const { readFromFile, writeToFile } = require("./helpers/fsUtils");
 const app = express();
 
+// Sets an initial port to use later in our listener
 const PORT = process.env.PORT || 3001;
 
+// Sets up the Express app to handle data parsing
 app.use(express.static(path.join(__dirname, "/public")));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let notes = [];
+//let notes = [];
 
 //Routes
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
@@ -28,10 +25,12 @@ app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+//Wild card
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//Body content
 app.post("/api/notes", (req, res) => {
   const newNote = {
     title: req.body.title,
@@ -50,7 +49,7 @@ app.delete("/api/notes/:id", (req, res) => {
   readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
     .then((json) => {
-      // Make a new array of all tips except the one with the ID provided in the URL
+      // Make a new array of all notes except the one with the ID provided in the URL
       const result = json.filter((tip) => tip.id !== tipId);
 
       // Save that array to the filesystem
@@ -61,7 +60,7 @@ app.delete("/api/notes/:id", (req, res) => {
     });
 });
 
-//starts the server to start listening
+//Starts the server to start listening at designated PORT
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
 });
