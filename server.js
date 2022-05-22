2a2496ce51da41af3bf4eb8af4e0605888a63543
Kym Reilly <kymreilly@hotmail.com>
2022-05-22 18:57:44 +0930

    created body structure for post, delete still not functional yet

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const uuid = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,13 +10,33 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes);
+});
+
+//Creates payload using uuid to generate an ID
+app.post("/api/notes", (req, res) => {
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  //Converting notes to object
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  notes.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 4));
+  res.json(notes);
 });
 
 app.get("*", (req, res) => {
